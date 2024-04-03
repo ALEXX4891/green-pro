3e11522bb4587e1c3c813060c585671990ec7ff8
il115 <web@ww.net.ru>
2024-04-03 17:58:20 +0500

    добавит верстку десктопа всех страниц

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
 // $(document).ready(function(){
 //   $(".hamburger").click(function(){
 //     $(this).toggleClass("is-active");
@@ -12,6 +26,8 @@ burder.addEventListener("click", function(){
   this.classList.toggle("burger_active");
   menu.classList.toggle("header__nav_active");
 });
+
+
 
 
 
