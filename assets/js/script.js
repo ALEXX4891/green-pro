b850c4fbb36a5309ee807003579378fe80c32887
ALEXX4891 <alexx4891@mail.ru>
2024-04-09 19:29:13 +0500

    добавит модальное окно - форму  на все страницы, фикс бага мерцания картинок партнеров

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,147 @@ burder.addEventListener("click", function(){
 });
 
 
+// popup:
+// const popupLink = document.querySelector('.popup-form');
+const popupLinks = document.querySelectorAll(".popup-link");
+const body = document.querySelector("body");
+// const popupBtn = document.querySelector('.popup-form__btn');
+const lockPadding = document.querySelectorAll(".lock-padding");
+const btn = document.querySelector(".project-btn");
+const form = document.querySelector("#popup-form");
+const successModal = document.querySelector("#success");
+const timeout = 800;
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  popupClose(form);
+
+  setTimeout(function () {
+    popupOpen(successModal);
+  },timeout)
+})
+
+let unlock = true;
+
+if (popupLinks.length > 0) {
+  for (let index = 0; index < popupLinks.length; index++) {
+    const popupLink = popupLinks[index];
+    popupLink.addEventListener("click", function (e) {
+      const popupName = popupLink.getAttribute("href").replace("#", "");
+      const curentPopup = document.getElementById(popupName); //получаем id попап-окна
+      popupOpen(curentPopup);
+      e.preventDefault();
+    });
+  }
+}
+
+// TODO прибраться в коде
+
+const popupCloseIcon = document.querySelectorAll(".popup-close");
+if (popupCloseIcon.length > 0) {
+  for (let index = 0; index < popupCloseIcon.length; index++) {
+    const el = popupCloseIcon[index];
+    el.addEventListener("click", function (e) {
+      popupClose(el.closest(".popup")); //ближайший родитель класса popup
+      e.preventDefault();
+    });
+  }
+}
+
+function popupOpen(curentPopup) {
+  if (curentPopup && unlock) {
+    const popupActive = document.querySelector(".popup.open");
+    if (popupActive) {
+      // закрываем текущий открытый попап, если он есть
+      popupClose(popupActive, false);      
+    } else {
+      bodyLock();
+    }
+    curentPopup.classList.add("open");
+    curentPopup.addEventListener("click", function (e) {
+      if (!e.target.closest(".popup__content")) {
+        // если клик был по области вокруг попапа то ничего не делаем
+        popupClose(e.target.closest(".popup"));
+      }
+    });
+  }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+  if (unlock) {
+    popupActive.classList.remove("open");    
+    if (doUnlock) {
+      bodyUnLock();
+    }
+  }
+}
+
+// добавляем боди padding-right при открытии попапа, на ширину скролл-бара
+function bodyLock() {
+  const lockPaddingValue =
+    window.innerWidth - document.querySelector(".header").offsetWidth + "px";
+  for (let index = 0; index < lockPadding.length; index++) {
+    const el = lockPadding[index];
+    el.style.marginRight = lockPaddingValue;
+  }
+  body.style.paddingRight = lockPaddingValue;
+  body.classList.add("lock");
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+
+function bodyUnLock() {
+  setTimeout(function () {
+    for (let index = 0; index < lockPadding.length; index++) {
+      const el = lockPadding[index];
+      el.style.marginRight = "0px";
+    }
+    body.style.paddingRight = "0px";
+    body.classList.remove("lock");
+  }, timeout);
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    const popupActive = document.querySelector(".popup.open");
+    popupClose(popupActive);
+  }
+});
+
+// полифилы:
+(function() {
+  if(!Element.prototype.closest) {
+    Element.prototype.closest = function (css) {
+      var node = this;
+      while(node) {
+        if (node.matches(css)) return node;
+        else node = node.parentElement;
+      }
+      return null;
+    };
+  }
+})();
+(function() {
+  if(!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.matchesSelector ||
+    Element.prototype.mozMatchesSelector ||
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.oMatchesSelector;
+  }
+})
+  
+
+
+
+
 
 
 
